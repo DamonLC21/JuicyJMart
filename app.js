47a6db19cc5eebe18cd97cf5e74c3ec5913f37db
Nicholas Lee <nicholaslee@Nicholass-MacBook-Air-2.local>
2018-09-24 13:07:59 -0600

    created server with app.js, json with depenedencies, and one get route

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const port = process.env.PORT || 5577
+app.listen(port, () => {
+    console.log(`wanna know what port is active you twerp? It's port ${port}`)
+})
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res, next) =>    {
+    
+})
