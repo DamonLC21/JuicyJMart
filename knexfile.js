52f9b1410985c91b3107dd830c7e5e94ef4081cc
Nicholas Lee <nicholaslee@Nicholass-MacBook-Air-2.local>
2018-09-25 10:52:16 -0600

    deleted pool object in knex.js file

--- knexfile.js
+++ knexfile.js
@@ -10,11 +10,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10
-    }
+    connection: process.env.DATABASE_URL
   }
 }
 
